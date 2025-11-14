d0544228265bf53c054a87e4c33a888a0815f79a
jayesh <jayeshsarvaiya9913@gmail.com>
2025-11-14 23:24:21 +0530

    FIX: mason plugins fix

--- lua/custom/configs/lsp.lua
+++ lua/custom/configs/lsp.lua
@@ -3,57 +3,36 @@ local lspconfig = require("lspconfig")
 local on_attach = base.on_attach
 local capabilities = base.capabilities
 
----------------------------------------------------------
--- TypeScript / JavaScript
----------------------------------------------------------
 lspconfig.ts_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- JSON
----------------------------------------------------------
 lspconfig.jsonls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- HTML
----------------------------------------------------------
 lspconfig.html.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- CSS
----------------------------------------------------------
 lspconfig.cssls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- ESLint
----------------------------------------------------------
 lspconfig.eslint.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- Tailwind CSS
----------------------------------------------------------
 lspconfig.tailwindcss.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
 
----------------------------------------------------------
--- Lua
----------------------------------------------------------
 lspconfig.lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
