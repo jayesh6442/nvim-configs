bb9044ab92969662026d33bf1e0df28846b5c9d8
jayesh <jayeshsarvaiya9913@gmail.com>
2026-01-09 18:20:10 +0530

    new changes

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,9 +1,19 @@
-local options = {
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  -- load default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- ❌ remove Ctrl+n at the source
+  vim.keymap.del("n", "<C-n>", { buffer = bufnr })
+end
+
+return {
   view = {
     side = "right",
     width = 30,
+    preserve_window_proportions = false,
   },
+  on_attach = on_attach,
 }
-
-return options
 
